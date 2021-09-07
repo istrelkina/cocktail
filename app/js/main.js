f2cf283b8ff11a969e3c9a022c0d0ceabf4f6d7c
istrelkina <istrelkina@gmail.com>
2021-09-07 21:54:34 +1000

    added section catalog and footer

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+
+$('.list__items').each(function() {
+	let ths = $(this);
+	ths.find('.item').not(':first').hide();
+	ths.find('.list__item').click(function() {
+		ths.find('.list__item').removeClass('active').eq($(this).index()).addClass('active');
+		ths.find('.item').hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass('active');
+
+	/*ths.find('.all-items-link').click(function() {
+		alert('Возникла');
+	});*/
+
+	});
+});
+
+$('.all-items-link').on('click', function() {
+	$('.list__items').each(function() {
+		let ths = $(this);
+		ths.find('.item').show();
+	});
+});
